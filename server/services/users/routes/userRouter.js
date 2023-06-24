8cad0b433e150808f74a71c967045201b6933866
william <laurencius.william@gmail.com>
2023-06-25 00:39:48 +0700

    feat : CRD on users

--- server/services/users/routes/userRouter.js
+++ server/services/users/routes/userRouter.js
@@ -2,4 +2,12 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/userController");
 
+router.get("/", UserController.allUser);
+
+router.post("/register", UserController.newUser);
+
+router.get("/:id", UserController.findById);
+
+router.delete("/:id", UserController.delete);
+
 module.exports = router;
