bd974b7935eb28877b407926a3a1c3959a9f88a2
william <laurencius.william@gmail.com>
2023-06-27 03:52:51 +0700

    deployment setup

--- server/orchestrator/config/redisConnection.js
+++ server/orchestrator/config/redisConnection.js
@@ -1,5 +1,8 @@
 const Redis = require("ioredis");
-
-const redis = new Redis();
-
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: "11873",
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
+});
 module.exports = redis;
